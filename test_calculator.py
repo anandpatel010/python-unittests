b0167c314f9cbb773c9112ba3cde0359a688c0d1
AP <nandopatel2001@yahoo.co.uk>
2024-08-02 22:07:30 +0100

    Add files via upload

---
+++ test_calculator.py
@@ -0,0 +1,32 @@
+import unittest
+from calculator import Calculator
+
+class TestCalculator(unittest.TestCase):
+    def setUp(self):
+        self.calc = Calculator()
+    
+    def test_add(self):
+        self.assertEqual(self.calc.add(1, 2), 3)
+        self.assertEqual(self.calc.add(-1, 1), 0)
+        self.assertEqual(self.calc.add(0, 0), 0)
+    
+    def test_subtract(self):
+        self.assertEqual(self.calc.subtract(10, 5), 5)
+        self.assertEqual(self.calc.subtract(2, 3), -1)
+        self.assertEqual(self.calc.subtract(0, 0), 0)
+    
+    def test_multiply(self):
+        self.assertEqual(self.calc.multiply(3, 4), 12)
+        self.assertEqual(self.calc.multiply(-1, 5), -5)
+        self.assertEqual(self.calc.multiply(0, 100), 0)
+    
+    def test_divide(self):
+        self.assertEqual(self.calc.divide(10, 2), 5)
+        self.assertEqual(self.calc.divide(-10, 2), -5)
+        with self.assertRaises(ValueError):
+            self.calc.divide(10, 0)
+        with self.assertRaises(ValueError):
+            self.calc.divide(0, 0)
+
+if __name__ == '__main__':
+    unittest.main()
